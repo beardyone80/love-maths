f1d6ca6daf5330556249df6526d3f1ae435f5b79
Paul Beardwell <beardwell@hotmail.com>
2023-10-19 11:22:35 +0000

    added code to incrememnt score counters

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Oh noooo you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -72,11 +74,23 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrememntScore() {
+/**
+ * Gets the current score from correct then adds 1
+ */
+function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current score from incorrect then adds 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
